7251bdf29e0f28a8f3361cf386a5078f130dd963
Sahibjot01 <sahibjot70@gmail.com>
2024-07-17 02:50:35 -0400

    chore: Add axios dependency and implement fetching popular games

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import { popularGamesURL } from "./api";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loadGames } from "./actions/gamesActions";
+
 function App() {
-  console.log(popularGamesURL());
+  const dispatch = useDispatch();
+  const { popular } = useSelector((state) => state.games);
+  useEffect(() => {
+    dispatch(loadGames());
+  }, [dispatch]);
   return (
     <>
-      <h1>{popularGamesURL()}</h1>
+      <h1>Popular Games</h1>
+      <ul>
+        {popular.map((game) => (
+          <li key={game.id}>{game.name}</li>
+        ))}
+      </ul>
     </>
   );
 }
