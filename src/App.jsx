ccf62939abdcbc075bc936bdaca572b81c54577a
Sahibjot01 <sahibjot70@gmail.com>
2024-07-17 19:18:54 -0400

    6. Refactor App component to use Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,12 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { loadGames } from "./actions/gamesActions";
+//component and pages
+import Home from "./pages/Home";
 
 function App() {
-  const dispatch = useDispatch();
-  const { popular, upcoming, newGames } = useSelector((state) => state.games);
-
-  useEffect(() => {
-    dispatch(loadGames());
-  }, [dispatch]);
   return (
-    <div>
-      <h1>Games</h1>
-      <div>
-        <h2>Popular Games</h2>
-        <ul>
-          {popular && popular.map((game) => <li key={game.id}>{game.name}</li>)}
-        </ul>
-      </div>
-      <div>
-        <h2>Upcoming Games</h2>
-        <ul>
-          {upcoming &&
-            upcoming.map((game) => <li key={game.id}>{game.name}</li>)}
-        </ul>
-      </div>
-      <div>
-        <h2>New Games</h2>
-        <ul>
-          {newGames &&
-            newGames.map((game) => <li key={game.id}>{game.name}</li>)}
-        </ul>
-      </div>
-    </div>
+    <>
+      <h1>Hello Ignite</h1>
+      <Home />
+    </>
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,39 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loadGames } from "../actions/gamesActions";
+
+const Home = () => {
+  const dispatch = useDispatch();
+  const { popular, upcoming, newGames } = useSelector((state) => state.games);
+
+  useEffect(() => {
+    dispatch(loadGames());
+  }, [dispatch]);
+  return (
+    <div>
+      <h1>Games</h1>
+      <div>
+        <h2>Popular Games</h2>
+        <ul>
+          {popular && popular.map((game) => <li key={game.id}>{game.name}</li>)}
+        </ul>
+      </div>
+      <div>
+        <h2>Upcoming Games</h2>
+        <ul>
+          {upcoming &&
+            upcoming.map((game) => <li key={game.id}>{game.name}</li>)}
+        </ul>
+      </div>
+      <div>
+        <h2>New Games</h2>
+        <ul>
+          {newGames &&
+            newGames.map((game) => <li key={game.id}>{game.name}</li>)}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
